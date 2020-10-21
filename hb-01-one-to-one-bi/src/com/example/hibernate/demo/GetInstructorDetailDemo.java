61ef920a93dffdea1d30c7601242c22ce2f25b4c
Pawan Saroj <pawansaroj6783@gmail.com>
2020-10-21 16:57:38 +0530

    one to one mapping bidirectional

---
+++ hb-01-one-to-one-bi/src/com/example/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,48 @@
+package com.example.hibernate.demo;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.example.hibernate.demo.entity.Instructor;
+import com.example.hibernate.demo.entity.InstructorDetail;
+import com.example.hibernate.demo.entity.Student;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		
+		try {
+			int id = 2;
+
+			session.beginTransaction();
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, id);
+			
+			if(tempInstructorDetail != null) {
+				System.out.println(tempInstructorDetail.getInstructor().toString());
+				session.getTransaction().commit();
+			}
+			else {
+				System.out.println("Object not found");
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} 
+		
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
